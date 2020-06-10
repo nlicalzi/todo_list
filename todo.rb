f57f18b1edbd395044dd713cafa772399f15ab83
nlicalzi <nicholas.licalzi@gmail.com>
2020-06-10 12:40:38 -0400

    added /lists/new path

--- todo.rb
+++ todo.rb
@@ -2,14 +2,25 @@ require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubis"
 
+configure do
+  enable :sessions
+  set :session_secret, 'secret' # change to a long hi-entropy env var
+end
+
+before do
+  session[:lists] ||= []
+end
+
 get "/" do
   redirect "/lists"
 end 
 
 get "/lists" do
-  @lists = [
-    {name: "Lunch Groceries", todos: []},
-    {name: "Dinner Groceries", todos: []}
-  ]
+  @lists = session[:lists]
   erb :lists, layout: :layout
+end
+
+get "/lists/new" do
+  session[:lists] << { name: "New List", todos: [] } # use a form for this
+  redirect "/lists"
 end

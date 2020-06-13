5b13c59e4587436340a3777167cee2271ca9b44c
nlicalzi <nicholas.licalzi@gmail.com>
2020-06-13 19:49:42 -0400

    added confirm functionality before deleting a list or a todo

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,1 +1,13 @@
-console.log("hello world")
+$(function() {
+
+  $("form.delete").submit(function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    var ok = confirm("Are you sure? This can't be undone!");
+    if (ok) {
+      this.submit();
+    }
+  });
+  
+});
